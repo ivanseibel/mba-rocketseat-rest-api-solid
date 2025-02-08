74366e8b8b6806bd2c9a2b3a0b7c56efe5d35529
Ivan Seibel <me@ivanseibel.dev>
2025-02-08 12:32:16 +0000

    feat: add profile controller and route for retrieving user profile

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { authenticate } from "@/http/controllers/authenticate";
+import { profile } from "@/http/controllers/profile";
 import type { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post("/users", register);
-
 	app.post("/sessions", authenticate);
+
+	/** Authenticated */
+	app.get("/me", profile);
 }
