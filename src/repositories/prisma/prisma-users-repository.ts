a0dd0ef0bb0ef70a0548fb0adf4a65023df4036b
Ivan Seibel <me@ivanseibel.dev>
2025-02-08 12:16:38 +0000

    feat: implement PrismaCheckInsRepository with methods for check-in retrieval and management; update PrismaUsersRepository to include user retrieval

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,11 +1,17 @@
 import { prisma } from "@/lib/prisma";
-import type { Prisma, User } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 
 import type { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
-	findById(id: string): Promise<User | null> {
-		throw new Error("Method not implemented.");
+	async findById(id: string) {
+		const user = await prisma.user.findUnique({
+			where: {
+				id,
+			},
+		});
+
+		return user;
 	}
 
 	async findByEmail(email: string) {
